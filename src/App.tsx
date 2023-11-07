d85a9122186145b0f100dbc081bdba0f56c03870
Quang Tran <nhatquang0672@gmail.com>
2023-11-07 23:31:04 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { UserProfile } from "./components/One/UserProfile";
+
+function App() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center bg-slate-100 underline">
+      <UserProfile />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/One/UserProfile.tsx
@@ -0,0 +1,7 @@
+export const UserProfile = () => {
+  return (
+    <div className="flex w-[35rem] rounded-lg bg-white p-12">
+      <img src="assets/react.svg"></img>
+    </div>
+  );
+};
